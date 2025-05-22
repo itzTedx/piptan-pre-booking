55840ca3f9e1383dfec44bddcb9c821ef23ecdfe
melwin <114172287+itzTedx@users.noreply.github.com>
2025-05-22 17:31:27 +0400

    refactor: Revise footer layout and enhance social media link styles
    - Updated footer structure to use a grid layout for better alignment and spacing.
    - Adjusted hover effects for social media links to improve visual feedback.
    - Integrated ScrollToTop component for enhanced user navigation.
    - Maintained copyright and developer attribution with improved styling for clarity.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,28 +7,20 @@ import {
 } from "@/assets/icons";
 import { Logo } from "@/assets/logo";
 
+import { ScrollToTop } from "./scroll-to-top";
+
 const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
   return (
     <footer
-      className="text-primary container max-w-7xl rounded-t-4xl bg-black px-12 shadow-2xl"
+      className="text-primary container max-w-7xl rounded-t-4xl bg-black shadow-2xl md:px-12"
       role="contentinfo"
     >
-      <div className="flex items-center justify-between gap-4 pt-12 pb-4">
+      <div className="grid grid-cols-3 gap-4 pt-12 pb-4">
         <Logo className="text-foreground" />
-        <p className="text-muted-foreground/75 text-sm">
-          Developed by{" "}
-          <Link
-            href="https://www.zironmedia.com"
-            target="_blank"
-            className="font-semibold hover:underline"
-            aria-label="Website Developed by Ziron media"
-          >
-            Ziron media
-          </Link>
-        </p>
-        <nav aria-label="Social Media Links">
+
+        <nav aria-label="Social Media Links" className="justify-self-center">
           <ul className="text-primary flex items-center gap-4">
             <li>
               <Link
@@ -36,7 +28,7 @@ export const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Visit our Facebook page"
-                className="transition-colors hover:text-green-900"
+                className="transition-colors hover:text-green-500"
               >
                 <IconBrandFacebook className="size-8" />
               </Link>
@@ -47,7 +39,7 @@ export const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Visit our Instagram profile"
-                className="transition-colors hover:text-green-900"
+                className="transition-colors hover:text-green-500"
               >
                 <IconBrandInstagram className="size-8" />
               </Link>
@@ -58,23 +50,35 @@ export const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Visit our LinkedIn page"
-                className="transition-colors hover:text-green-900"
+                className="transition-colors hover:text-green-500"
               >
                 <IconBrandLinkedin className="size-8" />
               </Link>
             </li>
           </ul>
         </nav>
+        <ScrollToTop />
       </div>
 
-      <div className="border-primary/20 flex flex-wrap items-center justify-between border-t py-2">
-        <p className="text-xs">
+      <div className="border-primary/20 grid grid-cols-1 items-center border-t py-2 md:grid-cols-3">
+        <p className="text-xs max-md:order-3 max-md:pb-1 max-md:text-center">
           © {currentYear}, Piptan Investment & Securities, All rights reserved
         </p>
-        <nav aria-label="Footer Navigation">
+        <p className="text-muted-foreground/75 text-center text-sm">
+          Developed by{" "}
+          <Link
+            href="https://www.zironmedia.com"
+            target="_blank"
+            className="font-semibold hover:underline"
+            aria-label="Website Developed by Ziron media"
+          >
+            Ziron media
+          </Link>
+        </p>
+        <nav aria-label="Footer Navigation" className="text-center md:text-end">
           <Link
             href="/privacy"
-            className="text-sm transition-colors hover:text-green-900"
+            className="text-sm transition-colors hover:text-green-500"
           >
             Privacy Policy
           </Link>
