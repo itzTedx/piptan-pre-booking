d2bfee03054ed24d8b07b4b6511ee6a08a2bad5b
Ziron Media <dev@zironmedia.com>
2026-02-23 15:33:34 +0400

    chore(config): update Next.js configuration and dependencies for improved performance

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		unoptimized: true,
+	reactCompiler: true,
+
+	experimental: {
+		turbopackFileSystemCacheForBuild: true,
+		turbopackFileSystemCacheForDev: true,
 	},
-	env: {
-		nextImageExportOptimizer_imageFolderPath: "public",
-		nextImageExportOptimizer_exportFolderPath: "out",
-		nextImageExportOptimizer_quality: "75",
-		nextImageExportOptimizer_storePicturesInWEBP: "true",
-		nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
-		nextImageExportOptimizer_generateAndUseBlurImages: "true",
-		nextImageExportOptimizer_remoteImageCacheTTL: "0",
-	},
+	// images: {
+	// 	unoptimized: true,
+	// },
+	// env: {
+	// 	nextImageExportOptimizer_imageFolderPath: "public",
+	// 	nextImageExportOptimizer_exportFolderPath: "out",
+	// 	nextImageExportOptimizer_quality: "75",
+	// 	nextImageExportOptimizer_storePicturesInWEBP: "true",
+	// 	nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+	// 	nextImageExportOptimizer_generateAndUseBlurImages: "true",
+	// 	nextImageExportOptimizer_remoteImageCacheTTL: "0",
+	// },
 };
 
 export default nextConfig;
